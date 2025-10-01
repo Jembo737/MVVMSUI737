6ed69a6055a5bb86c66fd67cf7c2a8e312a23c89
Jembo737 <74143229+Jembo737@users.noreply.github.com>
2025-10-01 22:25:06 +0400

    Inital commit.

---
+++ MVVMSUI737/MVVMSUI737/MVVMSUI737App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSUI737App.swift
+//  MVVMSUI737
+//
+//  Created by Dzhemal on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSUI737App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
